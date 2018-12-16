364eb8e063de29a8a16fc437ba250d1c28c76784
Unknown <contact@ilyt.cc>
2018-12-16 23:21:48 +0000

    msiign ;

--- inc/pages/CreateClan.php
+++ inc/pages/CreateClan.php
@@ -17,6 +17,6 @@ class CreateClan {
 		<div class="input-group"><span class="input-group-addon" id="basic-addon1"><span class="glyphicon glyphicon-user" max-width="25%"></span></span><input type="text" name="c" required class="form-control" placeholder="Clan Name" aria-describedby="basic-addon1"></div><p style="line-height: 15px"></p>
 		<div class="input-group"><span class="input-group-addon" id="basic-addon1"><span class="glyphicon glyphicon-user" max-width="25%"></span></span><input type="text" name="t" required class="form-control" placeholder="Clan Tag" aria-describedby="basic-addon1"></div><p style="line-height: 15px"></p>
 		<button type="submit" class="btn btn-primary">Create the clan!</button>
-		</form>'
+		</form>';
 	}
 }
