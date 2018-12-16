b096e1109b2b7afe4cf4be52d5757bfe0a84dcf2
Unknown <contact@ilyt.cc>
2018-12-16 23:23:57 +0000

    xd

--- inc/pages/CreateClan.php
+++ inc/pages/CreateClan.php
@@ -12,7 +12,7 @@ class CreateClan {
 		P::MaintenanceStuff();
 
 		echo "<h2> Create a new clan </h2>";
-		echo '<form action="/submit.php" method="POST"> 
+		echo '<form action="/submit.php&csrf='.csrfToken().'" method="POST"> 
 		<input name="action" value="createClan" hidden>
 		<div class="input-group"><span class="input-group-addon" id="basic-addon1"><span class="glyphicon glyphicon-user" max-width="25%"></span></span><input type="text" name="c" required class="form-control" placeholder="Clan Name" aria-describedby="basic-addon1"></div><p style="line-height: 15px"></p>
 		<div class="input-group"><span class="input-group-addon" id="basic-addon1"><span class="glyphicon glyphicon-user" max-width="25%"></span></span><input type="text" name="t" required class="form-control" placeholder="Clan Tag" aria-describedby="basic-addon1"></div><p style="line-height: 15px"></p>
