4f493c3a9b5badb74edef4cc15104622f5e4e9e2
ilyt <contact@ilyt.cc>
2019-01-02 15:06:45 +0000

    what about now?

--- inc/pages/getScreenShotsPage.php
+++ inc/pages/getScreenShotsPage.php
@@ -11,15 +11,16 @@ class Screenshots {
 		$screenshots = $GLOBALS["db"]->fetchAll("SELECT ssid FROM screenshots WHERE userid = ?", $userid);
 		//$c = 0;
         $user = $GLOBALS["db"]->fetchAll("SELECT username FROM users WHERE id = ?", $userid);
+        $username = $user["username"];
         P::GlobalAlert();
 		echo '
 		<div id="content">
 			<div align="center">
-                <h1><i class="fa fa-image"></i> '.$user[0].'\'s Screenshots </h1>
+                <h1><i class="fa fa-image"></i> '.$username.'\'s Screenshots </h1>
 				<div class="container">
 				';
             foreach ($screenshots as $j => $ss) {
-                echo $ss;
+                echo $ss["ssid"];
                 echo "<a href='https://yozora.pw/ss/'".$ss[0].".jpg'><img src='https://yozora.pw/ss/'".$ss[0].".jpg' /></a>";
             } // probably wont work lol
 			echo '</div>
